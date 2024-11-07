a8b8ad2e6e41865e7985003865527384929c4d5f
Visharad007 <117433856+Visharad007@users.noreply.github.com>
2024-11-07 17:20:05 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
 
 setup(
-    name="whrttheme",  # Replace with your app's name
+    name="whrttheme",  
     version="0.0.1",
     description="theme for ERPNext",
     author="Visharad",
